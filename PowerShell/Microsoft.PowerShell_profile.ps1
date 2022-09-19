fca51acbdd81858a253a9bd2d08c0af0a7fa6e04
Charlie Quick <cquickreal00@gmail.com>
2022-09-19 12:30:41 -0400

    Update Microsoft.PowerShell_profile.ps1
    Added Set-Wallpaper function and added #region / #endRegion support for collapsing sections for easier viewing.

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,22 +1,123 @@
-# Functions
+# =========
+# FUNCTIONS
+# =========
+#region
 function Get-Profile {
   & $PROFILE
 }
 
-# Aliases
-Set-Alias -Name ReloadProfile -Value Get-Profile
+function Set-Wallpaper {
+  param (
+    [string]$Path,
+    [ValidateSet('Tile', 'Center', 'Stretch', 'Fill', 'Fit', 'Span')]
+    [string]$Style = 'Fill'
+  )
 
+  begin {
+    try {
+      Add-Type @"
+              using System;
+              using System.Runtime.InteropServices;
+              using Microsoft.Win32;
+              namespace Wallpaper
+              {
+                  public enum Style : int
+                  {
+                    Tile, Center, Stretch, Fill, Fit, Span, NoChange
+                  }
+
+                  public class Setter
+                  {
+                    public const int SetDesktopWallpaper = 20;
+                    public const int UpdateIniFile = 0x01;
+                    public const int SendWinIniChange = 0x02;
+                    [DllImport( "user32.dll", SetLastError = true, CharSet = CharSet.Auto )]
+                    private static extern int SystemParametersInfo ( int uAction, int uParam, string lpvParam, int fuWinIni );
+                    public static void SetWallpaper ( string path, Wallpaper.Style style )
+                      {
+                      SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+                      RegistryKey key = Registry.CurrentUser.OpenSubKey( "Control Panel\\Desktop", true );
+                      switch( style )
+                      {
+                        case Style.Tile :
+                        key.SetValue( @"WallpaperStyle", "0" ) ;
+                        key.SetValue( @"TileWallpaper", "1" ) ;
+                        break;
+                        case Style.Center :
+                        key.SetValue( @"WallpaperStyle", "0" ) ;
+                        key.SetValue( @"TileWallpaper", "0" ) ;
+                        break;
+                        case Style.Stretch :
+                        key.SetValue( @"WallpaperStyle", "2" ) ;
+                        key.SetValue( @"TileWallpaper", "0" ) ;
+                        break;
+                        case Style.Fill :
+                        key.SetValue( @"WallpaperStyle", "10" ) ;
+                        key.SetValue( @"TileWallpaper", "0" ) ;
+                        break;
+                        case Style.Fit :
+                        key.SetValue( @"WallpaperStyle", "6" ) ;
+                        key.SetValue( @"TileWallpaper", "0" ) ;
+                        break;
+                        case Style.Span :
+                        key.SetValue( @"WallpaperStyle", "22" ) ;
+                        key.SetValue( @"TileWallpaper", "0" ) ;
+                        break;
+                        case Style.NoChange :
+                        break;
+                      }
+                      key.Close();
+                    }
+                  }
+              }
+"@
+    }
+    catch {}
+
+    $StyleNum = @{
+      Tile    = 0
+      Center  = 1
+      Stretch = 2
+      Fill    = 3
+      Fit     = 4
+      Span    = 5
+    }
+  }
+
+  process {
+    [Wallpaper.Setter]::SetWallpaper($Path, $StyleNum[$Style])
+
+    # sometimes the wallpaper only changes after the second run, so I'll run it twice!
+    Start-Sleep -ms 200
+    [Wallpaper.Setter]::SetWallpaper($Path, $StyleNum[$Style])
+  }
+}
+#endRegion
+
+# =======
+# ALIASES
+# =======
+#region
+Set-Alias -Name ReloadProfile -Value Get-Profile
+Set-Alias -Name Wallpaper -Value Set-Wallpaper
+#endRegion
+
+# =======
+# STARTUP
+# =======
+#region
 Invoke-Expression (&starship init powershell)
 Set-Location ~
 Clear-Host
 Get-Content "~\Documents\PowerShell\banner.txt"
+#endRegion
 
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
+# ==========
+# CHOCOLATEY
+# ==========
+#region
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+#endRegion
